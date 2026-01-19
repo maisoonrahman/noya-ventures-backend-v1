9c20a41641134ca8e5ba6b978e7926c86f28e4e2
maisoonrahman <maisoon.a.r01@gmail.com>
2026-01-19 14:37:38 -0500

    changed config.py location

--- wsgi.py
+++ wsgi.py
@@ -1,8 +1,5 @@
-from dotenv import load_dotenv
-load_dotenv()  # optional, only if you keep a .env in prod
-
 from app import create_app
-from config import Config
+from app.config import Config
 
 app = create_app(Config)
-print("DB_HOST =", app.config.get("DB_HOST"))
+
