02d59ab87f0b3aa5b8e0e647030753a0db93bc8e
maisoonrahman <maisoon.a.r01@gmail.com>
2026-01-19 14:27:36 -0500

    changed create_app to explicitly load config

--- app/__init__.py
+++ app/__init__.py
@@ -1,30 +1,26 @@
 from flask import Flask
 from flask_cors import CORS
-from app.models.db import get_db, close_db
+from app.models.db import close_db
+from app.config import Config
 
-def create_app(config_object=None):
+def create_app():
     app = Flask(__name__, instance_relative_config=True)
 
-    if config_object:
-        app.config.from_object(config_object)
+    # loading config
+    app.config.from_object(Config)
 
     CORS(app)
 
     app.teardown_appcontext(close_db)
 
-    # Registering minimal blueprints for app to work
-    from app.routes.documents import documents_bp
     from app.routes.users import users_bp
+    from app.routes.documents import documents_bp
     from app.routes.startups import startups_bp
-
-    # test blueprint
     from app.routes.test import test_bp
 
     app.register_blueprint(users_bp)
     app.register_blueprint(documents_bp)
     app.register_blueprint(startups_bp)
-
-    # test blueprint
     app.register_blueprint(test_bp)
 
     return app
--- wsgi.py
+++ wsgi.py
@@ -1,2 +1,8 @@
+from dotenv import load_dotenv
+load_dotenv()  # optional, only if you keep a .env in prod
+
 from app import create_app
-app = create_app()
+from config import Config
+
+app = create_app(Config)
+print("DB_HOST =", app.config.get("DB_HOST"))
